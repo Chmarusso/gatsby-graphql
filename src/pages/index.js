ce0c59d4d9bb7c5c5ad2ad8c9b3a8f76b916ac5b
Artur Ch <chmarus@gmail.com>
2019-01-22 15:57:06 +0100

    init

---
+++ src/pages/index.js
@@ -0,0 +1,39 @@
+import React from 'react'
+import Layout from '../components/layout'
+import SEO from '../components/seo'
+
+import { graphql } from 'gatsby'
+
+const IndexPage = ({data}) => {
+
+  console.log(data)
+
+  return (
+    <Layout>
+      <SEO title="Home" keywords={[`gatsby`, `application`, `react`]} />
+      {data.strapi.menuitems.map(item =>
+        <div class="flex flex-column mb3 w-50-ns">
+          <div class="flex">
+            <div class="w-50">{item.title}</div>
+            <div class="w-50 tr">{item.price / 100},-</div>
+          </div>
+          <div class="w-100">
+            {item.description}
+          </div>
+        </div>
+      )}
+    </Layout>
+  )
+}
+
+export default IndexPage
+
+export const query = graphql`
+query {
+  strapi {
+    menuitems {
+      id, title, description, category, price
+    }
+  }
+}
+`
